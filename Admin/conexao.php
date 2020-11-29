ac5ed230aa2f6de985ac2ac4aceeef3ec849c8e1
devLeSilverio <leticia.silverio09@gmail.com>
2020-11-29 16:14:28 -0300

    add template

--- Admin/conexao.php
+++ Admin/conexao.php
@@ -36,10 +36,82 @@ function ListarUsuario(){
   return $res;
 }
 
+//---------------------------tb_info_adicionais--------------------------
+
+function CadastrarInfoAdicionais($informacoes,$id_usuario){
+  $sql = 'INSERT INTO tb_info_adicionais VALUES(null,"'.$informacoes.'",'.$id_usuario.')';
+  $res = $GLOBALS['conexao']->query($sql);
+  if($res){
+    alert("Informações cadastradas com sucesso!");
+  }
+  else{
+     alert("Informações não cadastradas".$GLOBALS['conexao']->error);
+  }
+}
+
+function ListarInfoAdicionais(){
+  $sql = 'SELECT * from tb_info_adicionais';
+  $res = $GLOBALS['conexao']->query($sql);
+  return $res;
+}
+
+//------------------------------------tb_formacao_academica 
+
+function CadastrarFormacao($nm_curso,$nm_instituicao,$dt_ano_conclusao,$id_usuario){
+  $sql = 'INSERT INTO tb_info_adicionais VALUES(null,"'.$nm_curso.'","'.$nm_instituicao.'","'.$dt_ano_conclusao.'",'.$id_usuario.')';
+  $res = $GLOBALS['conexao']->query($sql);
+  if($res){
+    alert("Informações cadastradas com sucesso!");
+  }
+  else{
+     alert("Informações não cadastradas".$GLOBALS['conexao']->error);
+  }
+}
+
+function ListarFormacao(){
+  $sql = 'SELECT * FROM tb_formacao_academica';
+  $res= $GLOBALS['conexao']->query($sql);
+  return $res;
 
 
+}
 
+//--------------------tb_localizacao
+function CadastrarLocalização($estado,$cidade,$cep,$id_usuario){
+  $sql = 'INSERT INTO tb_localizacao VALUES(null, "'.$estado.'","'.$cidade.'","'.$cep.'",'.$id_usuario.')';
+  $res = $GLOBALS['conexao']->query($sql);
+  if($res){
+    alert("Informações cadastradas com sucesso!");
+  }
+  else{
+     alert("Informações não cadastradas".$GLOBALS['conexao']->error);
+  }
+}
 
+function ListarLocalizaca(){
+
+  
+  $sql = 'SELECT * FROM tb_localizacao';
+  $res= $GLOBALS['conexao']->query($sql);
+  return $res;
+}
+
+//---------------------tb_experiencia
+function CadastrarExperencia($nm_empresa,$dt_inicio,$dt_fim,$cargo,$funcao_cargo,$atividades_complementares,$id_usuario){
+$sql = 'INSERT INTO tb_experiencia VALUES(null,"'.$nm_empresa.'","'.$dt_inicio.'","'.$dt_fim.'","'.$cargo.'","'.$funcao_cargo.'","'.$atividades_complementares.'",'.$id_usuario.')';
+if($res){
+  alert("Informações cadastradas com sucesso!");
+}
+else{
+   alert("Informações não cadastradas".$GLOBALS['conexao']->error);
+}
+}
+
+function ListarExperiencia (){
+  $sql = 'SELECT * FROM tb_experiencia';
+  $res= $GLOBALS['conexao']->query($sql);
+  return $res;
+}
 
 //--------------Complementares
 
@@ -49,3 +121,4 @@ function alert($msg){
 
 
 ?>
+
